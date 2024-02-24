e68485aa4d6bf09752a3e308a2d1b3f78ddaba56
abusayed0 <abusayed48876@gmail.com>
2024-02-24 17:41:03 +0600

    setup redux-toolkit

---
+++ src/redux/features/tasks/tasksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+tasks: []
+};
+const tasksSlice = createSlice({
+    name: "tasks",
+    initialState,
+    reducers: {
+        addTask : (state, action) => {
+
+        },
+        deleteTask : (state, action) => {
+
+        },
+    },
+
+});
+
+export const {addTask, deleteTask} = tasksSlice.actions;
+
+export default tasksSlice.reducer;
